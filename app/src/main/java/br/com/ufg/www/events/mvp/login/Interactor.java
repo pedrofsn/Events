5882fcd375d5c0019c65a369e25accedb86a5df6
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-05-02 23:51:03 -0300

    Adicionando a referência do presenter no interactor

--- app/src/main/java/br/com/ufg/www/events/mvp/login/Interactor.java
+++ app/src/main/java/br/com/ufg/www/events/mvp/login/Interactor.java
@@ -4,6 +4,12 @@ import br.com.ufg.www.events.model.Login;
 
 public class Interactor implements Contract.Interactor {
 
+    private Contract.Presenter presenter;
+
+    public Interactor(Contract.Presenter presenter) {
+        this.presenter = presenter;
+    }
+
     @Override
     public void login(Login login) {
         // Disparar requisição
