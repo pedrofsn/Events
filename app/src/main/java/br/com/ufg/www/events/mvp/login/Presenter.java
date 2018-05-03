11383a10bfe9bd883d9804f2f10c1ff246c21c11
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-05-02 23:51:53 -0300

    Adicionando a referência e chamada do interactor no presenter.
    Mas não sei se esta abordagem será interessante neste projeto, provavelmente vamos refatorar por conta dos requisitos que o mesmo irá exigir...

--- app/src/main/java/br/com/ufg/www/events/mvp/login/Presenter.java
+++ app/src/main/java/br/com/ufg/www/events/mvp/login/Presenter.java
@@ -5,6 +5,7 @@ import br.com.ufg.www.events.model.Login;
 public class Presenter implements Contract.Presenter {
 
     private Contract.View view;
+    private Interactor interactor = new Interactor(this);
 
     public Presenter(Contract.View view) {
         this.view = view;
@@ -12,6 +13,7 @@ public class Presenter implements Contract.Presenter {
 
     @Override
     public void login(Login login) {
+        interactor.login(login);
         if (("teste".equals(login.getLogin())) && ("teste".equals(login.getPassword()))) {
             view.onLoggedIn();
         } else {
