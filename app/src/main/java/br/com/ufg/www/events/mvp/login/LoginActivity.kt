5b28bea684b6718c2de318367dea80ad60b89052
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-05-29 00:13:44 -0300

    Migrando activity de login de java para kotlin

---
+++ app/src/main/java/br/com/ufg/www/events/mvp/login/LoginActivity.kt
@@ -0,0 +1,45 @@
+package br.com.ufg.www.events.mvp.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import br.com.ufg.www.events.R
+import br.com.ufg.www.events.domain.BaseActivity
+import br.com.ufg.www.events.model.Login
+import br.com.ufg.www.events.mvp.maps.List_Places_Activity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : BaseActivity(), Contract.View, View.OnClickListener {
+
+    private val presenter = Presenter(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        btnLogar.setOnClickListener(this)
+    }
+
+    override fun login() {
+        val loginString = edtLogin!!.text.toString()
+        val passwordString = edtSenha!!.text.toString()
+
+        val sing_in = Login(loginString, passwordString)
+        presenter.login(sing_in)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnLogar -> login()
+        }
+    }
+
+    override fun onLoggedIn() {
+        val intent = Intent(this, List_Places_Activity::class.java)
+        startActivity(intent)
+    }
+
+    override fun loginFailed() {
+        showMessage("Usuário ou senhas inválidos")
+    }
+
+}
