7526ba2dd2b9a29fd09982b3c0332e882661983c
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-05-29 00:05:38 -0300

    Migrando o presenter de Java pra Kotlin.

---
+++ app/src/main/java/br/com/ufg/www/events/mvp/login/Presenter.kt
@@ -0,0 +1,20 @@
+package br.com.ufg.www.events.mvp.login
+
+import br.com.ufg.www.events.model.Login
+
+class Presenter(private val view: Contract.View) : Contract.Presenter {
+
+    private val interactor = Interactor()
+
+    override fun login(login: Login) {
+        val callback = { result: Boolean ->
+            if (result) {
+                view.onLoggedIn()
+            } else {
+                view.loginFailed()
+            }
+        }
+
+        interactor.login(login, callback).start()
+    }
+}
