e0ad68c9470e141ef08c45d5107ff4ae3ab2cd13
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-05-29 00:21:51 -0300

    Deixando a activity com uma escrita "kotlin-way" (equivalente, com mais cara de Kotlin)

--- app/src/main/java/br/com/ufg/www/events/mvp/login/LoginActivity.kt
+++ app/src/main/java/br/com/ufg/www/events/mvp/login/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import br.com.ufg.www.events.R
 import br.com.ufg.www.events.domain.BaseActivity
+import br.com.ufg.www.events.extensions.getString
 import br.com.ufg.www.events.model.Login
 import br.com.ufg.www.events.mvp.maps.List_Places_Activity
 import kotlinx.android.synthetic.main.activity_login.*
@@ -20,11 +21,11 @@ class LoginActivity : BaseActivity(), Contract.View, View.OnClickListener {
     }
 
     override fun login() {
-        val loginString = editTextLogin.text.toString()
-        val passwordString = editTextPassword.text.toString()
+        val loginString = editTextLogin.getString()
+        val passwordString = editTextPassword.getString()
 
-        val sing_in = Login(loginString, passwordString)
-        presenter.login(sing_in)
+        val login = Login(loginString, passwordString)
+        presenter.login(login)
     }
 
     override fun onClick(v: View) {
@@ -33,13 +34,8 @@ class LoginActivity : BaseActivity(), Contract.View, View.OnClickListener {
         }
     }
 
-    override fun onLoggedIn() {
-        val intent = Intent(this, List_Places_Activity::class.java)
-        startActivity(intent)
-    }
+    override fun onLoggedIn() = startActivity(Intent(this, List_Places_Activity::class.java))
 
-    override fun loginFailed() {
-        showMessage("Usuário ou senhas inválidos")
-    }
+    override fun loginFailed() = showMessage("Usuário ou senhas inválidos")
 
 }
