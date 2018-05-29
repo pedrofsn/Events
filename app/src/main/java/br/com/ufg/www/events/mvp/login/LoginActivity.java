6442748912cb67013ddf2f13194fa1468806e1bc
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-05-29 00:12:01 -0300

    Removendo método não utilizado

--- app/src/main/java/br/com/ufg/www/events/mvp/login/LoginActivity.java
+++ app/src/main/java/br/com/ufg/www/events/mvp/login/LoginActivity.java
@@ -1,11 +1,9 @@
 package br.com.ufg.www.events.mvp.login;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -41,14 +39,6 @@ public class LoginActivity extends BaseActivity implements Contract.View, View.O
 
         Login sing_in = new Login(loginString, passwordString);
         presenter.login(sing_in);
-    }
-
-    private void hideKeyboard() {
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
     }
 
     @Override
