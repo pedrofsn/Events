7652a0cafc42cd09ccf0ce52cf935f290a33bd6e
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-05-29 00:05:22 -0300

    Requisição assíncrona na consulta no banco de dados, escrita de forma síncrona, graças ao coroutines.

--- app/src/main/java/br/com/ufg/www/events/mvp/login/Interactor.kt
+++ app/src/main/java/br/com/ufg/www/events/mvp/login/Interactor.kt
@@ -2,9 +2,17 @@ package br.com.ufg.www.events.mvp.login
 
 import br.com.ufg.www.events.database.AppDatabase
 import br.com.ufg.www.events.model.Login
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 
 class Interactor {
 
-    fun login(login: Login) = AppDatabase.getInstance().userDAO().isLoginValid(email = login.login, passwordUpperAndHashed = login.password)
+    fun login(login: Login, callback: ((Boolean) -> Unit)) = launch(UI) {
+        val deferred = async(CommonPool) { AppDatabase.getInstance().userDAO().isLoginValid(email = login.login, passwordUpperAndHashed = login.password) }
+        val result = deferred.await()
+        callback.invoke(result)
+    }
 
 }
