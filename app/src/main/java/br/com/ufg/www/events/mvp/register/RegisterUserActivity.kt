42ab8628015f411a79f81fa774e8f89c764b9af1
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-15 23:42:31 -0300

    - Validando form de registro do usuário com as estrutura de dados do when - Caso de sucesso após cadastro do usuário realizado

--- app/src/main/java/br/com/ufg/www/events/mvp/register/RegisterUserActivity.kt
+++ app/src/main/java/br/com/ufg/www/events/mvp/register/RegisterUserActivity.kt
@@ -1,11 +1,22 @@
 package br.com.ufg.www.events.mvp.register
 
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.os.Bundle
 import android.view.View
 import br.com.ufg.www.events.R
 import br.com.ufg.www.events.domain.BaseActivity
+import br.com.ufg.www.events.extensions.getString
+import br.com.ufg.www.events.extensions.isFilled
+import br.com.ufg.www.events.model.RegisterUser
+import br.com.ufg.www.events.mvp.maps.List_Places_Activity
+import kotlinx.android.synthetic.main.activity_register_user.*
 
 class RegisterUserActivity : BaseActivity(), Contract.View, View.OnClickListener {
+
+    private val presenter by lazy { Presenter(this) }
+    private val MINIMAL_LENGTH_PASSWORD = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +30,27 @@ class RegisterUserActivity : BaseActivity(), Contract.View, View.OnClickListener
             val password = editTextPassword.getString()
             val passwordConfirmation = editTextPasswordConfirmation.getString()
 
+            when {
+                password.length < MINIMAL_LENGTH_PASSWORD -> showMessage(String.format(getString(R.string.passwords_need_minimal_x_characters), MINIMAL_LENGTH_PASSWORD))
+                password != passwordConfirmation -> showMessage(getString(R.string.password_doesnt_match))
+                else -> {
+                    val registerUser = RegisterUser(
+                            login = login,
+                            password = password
+                    )
+
+                    presenter.register(registerUser)
+                }
+            }
 
         }
+    }
+
+    override fun onRegistered() {
+        val intent = Intent(this, List_Places_Activity::class.java)
+        intent.addFlags(FLAG_ACTIVITY_CLEAR_TASK)
+        intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 
     override fun onClick(v: View?) {
