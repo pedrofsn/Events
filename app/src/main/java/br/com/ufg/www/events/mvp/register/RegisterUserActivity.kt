d19fa33f65a5905fcfba20b4ca9cff68b1f35188
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-15 23:04:12 -0300

    Convertendo a classe java para kotlin ( CTRL + ALT + SHIFT + K )

---
+++ app/src/main/java/br/com/ufg/www/events/mvp/register/RegisterUserActivity.kt
@@ -0,0 +1,14 @@
+package br.com.ufg.www.events.mvp.register
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import br.com.ufg.www.events.R
+
+class RegisterUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
