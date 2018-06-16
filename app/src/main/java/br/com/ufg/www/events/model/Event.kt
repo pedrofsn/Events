831ce313a6bc5b14a507ac9dfa117920a418cb21
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 17:22:10 -0300

    Atualizando o modelo de evento

--- app/src/main/java/br/com/ufg/www/events/model/Event.kt
+++ app/src/main/java/br/com/ufg/www/events/model/Event.kt
@@ -1,9 +1,13 @@
 package br.com.ufg.www.events.model
 
+import br.com.ufg.www.events.App
+import br.com.ufg.www.events.R
+
 data class Event(
         val latitude: Double,
         val longitude: Double,
-        val description: String?
+        val description: String?,
+        val userLogin: String
 ) {
-    override fun toString() = "$description - local http://maps.googleapis.com/maps/api/geocode/xml?latlng=$latitude,$longitude"
+    override fun toString() = "$description\n${App.instance.getString(R.string.latitude)}: $latitude\t${App.instance.getString(R.string.longitude)}: $longitude"
 }
