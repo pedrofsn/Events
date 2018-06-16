a83515de9842d3d789a2fa6e7a39ade685304557
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 11:24:55 -0300

    - Extendendo a AppCompatActivity para usufruir dos recursos e retrocompatibilidade da lib de suporte v7

--- app/src/main/java/br/com/ufg/www/events/domain/BaseActivity.kt
+++ app/src/main/java/br/com/ufg/www/events/domain/BaseActivity.kt
@@ -1,10 +1,10 @@
 package br.com.ufg.www.events.domain
 
-import android.app.Activity
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 
-abstract class BaseActivity : Activity(), UIFeedback {
+abstract class BaseActivity : AppCompatActivity(), UIFeedback {
 
     override fun showMessage(message: String) = Snackbar.make(findViewById<View>(android.R.id.content), message, Snackbar.LENGTH_SHORT).show()
 
