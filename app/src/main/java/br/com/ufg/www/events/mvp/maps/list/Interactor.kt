7452006b60dd0c757a9e2a9f39d881eb5f3269e8
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 17:24:30 -0300

    Criando o acesso aos dados do db, para a os eventos.

---
+++ app/src/main/java/br/com/ufg/www/events/mvp/maps/list/Interactor.kt
@@ -0,0 +1,18 @@
+package br.com.ufg.www.events.mvp.maps.list
+
+import br.com.ufg.www.events.database.AppDatabase
+import br.com.ufg.www.events.model.Event
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+
+class Interactor {
+
+    fun load(userLogin: String, callback: ((List<Event>) -> Unit)) = launch(UI) {
+        val deferred = async(CommonPool) { AppDatabase.getInstance().eventDAO().readAll(userLogin = userLogin) }
+        val result = deferred.await()
+        callback.invoke(result)
+    }
+
+}
