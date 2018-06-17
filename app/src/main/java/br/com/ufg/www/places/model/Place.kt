966e39a84012df020961b4533972a1b318738831
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 22:23:08 -0300

    Transformando o Place em parcelable e adicionando método para mapear pra entidade

--- app/src/main/java/br/com/ufg/www/places/model/Place.kt
+++ app/src/main/java/br/com/ufg/www/places/model/Place.kt
@@ -1,14 +1,25 @@
 package br.com.ufg.www.places.model
 
+import android.os.Parcelable
 import br.com.ufg.www.places.App
 import br.com.ufg.www.places.R
+import br.com.ufg.www.places.database.entities.PlaceEntity
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Place(
-        val id: Long,
+        val id: Long = 0,
         val latitude: Double,
         val longitude: Double,
         val description: String?,
         val login: String
-) {
+) : Parcelable {
     override fun toString() = "$description\n${App.instance.getString(R.string.latitude)}: $latitude\t${App.instance.getString(R.string.longitude)}: $longitude"
+
+    fun toEntity() = PlaceEntity(
+            latitude = latitude,
+            longitude = longitude,
+            description = description,
+            userLogin = login
+    )
 }
