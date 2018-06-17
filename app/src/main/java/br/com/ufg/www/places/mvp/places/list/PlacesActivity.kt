11bd576c7034c7d4eab35cca3512834befd840c1
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 22:24:09 -0300

    - Adicionando fab button - Adicionando o layout manager no recyclerview (estava faltando) - Carregando os dados o banco - Enviando o objeto PLACE tocado para a próxima activity

--- app/src/main/java/br/com/ufg/www/places/mvp/places/list/PlacesActivity.kt
+++ app/src/main/java/br/com/ufg/www/places/mvp/places/list/PlacesActivity.kt
@@ -1,11 +1,15 @@
 package br.com.ufg.www.places.mvp.places.list
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import br.com.ufg.www.places.R
 import br.com.ufg.www.places.domain.BaseActivity
 import br.com.ufg.www.places.domain.MyOnItemClickListener
 import br.com.ufg.www.places.model.Place
+import br.com.ufg.www.places.mvp.places.LocationActivity
 import br.com.ufg.www.places.mvp.places.list.adapter.AdapterPlace
+import br.com.ufg.www.places.mvp.places.register.RegisterPlaceActivity
 import kotlinx.android.synthetic.main.activity_places.*
 
 class PlacesActivity : BaseActivity(), MyOnItemClickListener<Place>, Contract.View {
@@ -16,13 +20,22 @@ class PlacesActivity : BaseActivity(), MyOnItemClickListener<Place>, Contract.Vi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_places)
+        recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
+        fab.setOnClickListener { startActivity(Intent(this@PlacesActivity, RegisterPlaceActivity::class.java)) }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        load()
     }
 
     override fun load() = presenter.load()
 
     override fun openInMaps(place: Place) {
-
+        val intent = Intent(this, LocationActivity::class.java)
+        intent.putExtra("place", place)
+        startActivity(intent)
     }
 
     override fun onLoaded(places: List<Place>) = adapter.setCustomList(places)
