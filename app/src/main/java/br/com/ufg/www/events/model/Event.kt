339f43e501de2e43c1d4550865559ca08de6eb1b
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 21:39:59 -0300

    Adicionando o ID e username nno modelo

--- app/src/main/java/br/com/ufg/www/events/model/Event.kt
+++ app/src/main/java/br/com/ufg/www/events/model/Event.kt
@@ -4,10 +4,11 @@ import br.com.ufg.www.events.App
 import br.com.ufg.www.events.R
 
 data class Event(
+        val id: Long,
         val latitude: Double,
         val longitude: Double,
         val description: String?,
-        val userLogin: String
+        val login: String
 ) {
     override fun toString() = "$description\n${App.instance.getString(R.string.latitude)}: $latitude\t${App.instance.getString(R.string.longitude)}: $longitude"
 }
