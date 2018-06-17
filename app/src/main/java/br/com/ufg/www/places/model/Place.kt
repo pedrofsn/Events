c6b1f9dab85f1d31508fe361e92f30131856d3c3
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 22:35:05 -0300

    Não mais expondo o ID do banco ao modelo

--- app/src/main/java/br/com/ufg/www/places/model/Place.kt
+++ app/src/main/java/br/com/ufg/www/places/model/Place.kt
@@ -8,7 +8,6 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Place(
-        val id: Long = 0,
         val latitude: Double,
         val longitude: Double,
         val description: String?,
