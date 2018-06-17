cce76a9df9e95855795b34913d3fe887d60cdddb
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 21:40:36 -0300

    Corrigindo a referência do DAO

--- app/src/main/java/br/com/ufg/www/events/database/dao/EventDao.kt
+++ app/src/main/java/br/com/ufg/www/events/database/dao/EventDao.kt
@@ -2,15 +2,14 @@ package br.com.ufg.www.events.database.dao
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Query
-import br.com.ufg.www.events.database.entities.UserEntity
-import br.com.ufg.www.events.model.Event
+import br.com.ufg.www.events.database.entities.EventEntity
 import org.intellij.lang.annotations.Language
 
 @Dao
-interface EventDao : BaseDAO<UserEntity> {
+interface EventDao : BaseDAO<EventEntity> {
 
     @Language("RoomSql")
     @Query("SELECT * FROM events where login like :userLogin")
-    fun readAll(userLogin: String): List<Event>
+    fun readAll(userLogin: String): List<EventEntity>
 
 }
