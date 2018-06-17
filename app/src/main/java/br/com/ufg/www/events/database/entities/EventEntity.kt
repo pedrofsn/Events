99f753caf8d31bc4a04cd12c89337dcf3da7487c
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 21:40:16 -0300

    Adicionando um método que "converte/exporta" a entidade para o modelo

--- app/src/main/java/br/com/ufg/www/events/database/entities/EventEntity.kt
+++ app/src/main/java/br/com/ufg/www/events/database/entities/EventEntity.kt
@@ -3,6 +3,7 @@ package br.com.ufg.www.events.database.entities
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import br.com.ufg.www.events.model.Event
 
 
 @Entity(tableName = "events")
@@ -12,4 +13,12 @@ class EventEntity(
         val longitude: Double,
         val description: String?,
         @ColumnInfo(name = "login") val userLogin: String
-)
+) {
+    fun toModel() = Event(
+            id = id,
+            latitude = latitude,
+            longitude = longitude,
+            description = description,
+            login = userLogin
+    )
+}
