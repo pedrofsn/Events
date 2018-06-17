4e1c840bce7d1753b3c0e049bde2f2bedb97c011
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 22:17:25 -0300

    Adicionando método para que o home button ("setinha pra voltar") da toolbar, execute o método do onBackPressed.

--- app/src/main/java/br/com/ufg/www/places/domain/BaseActivity.kt
+++ app/src/main/java/br/com/ufg/www/places/domain/BaseActivity.kt
@@ -2,10 +2,18 @@ package br.com.ufg.www.places.domain
 
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import android.view.View
 
 abstract class BaseActivity : AppCompatActivity(), UIFeedback {
 
     override fun showMessage(message: String) = Snackbar.make(findViewById<View>(android.R.id.content), message, Snackbar.LENGTH_SHORT).show()
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (android.R.id.home == item?.itemId) {
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
