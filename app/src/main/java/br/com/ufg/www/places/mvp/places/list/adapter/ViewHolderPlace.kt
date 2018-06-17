edcf72f45e08bf6fc09dd3018c212dfb56747680
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-16 22:34:52 -0300

    Customizando o adapter de locais

--- app/src/main/java/br/com/ufg/www/places/mvp/places/list/adapter/ViewHolderPlace.kt
+++ app/src/main/java/br/com/ufg/www/places/mvp/places/list/adapter/ViewHolderPlace.kt
@@ -2,16 +2,21 @@ package br.com.ufg.www.places.mvp.places.list.adapter
 
 import android.view.View
 import android.widget.TextView
+import br.com.ufg.www.places.App
 import br.com.ufg.www.places.R
 import br.com.ufg.www.places.domain.BaseViewHolder
 import br.com.ufg.www.places.model.Place
 
 class ViewHolderPlace(itemView: View) : BaseViewHolder<Place>(itemView) {
 
-    private val textView by lazy { itemView.findViewById<TextView>(R.id.textView) }
+    private val textViewDescription by lazy { itemView.findViewById<TextView>(R.id.textViewDescription) }
+    private val textViewLatitude by lazy { itemView.findViewById<TextView>(R.id.textViewLatitude) }
+    private val textViewLongitude by lazy { itemView.findViewById<TextView>(R.id.textViewLongitude) }
 
     override fun popular(obj: Place) {
-        textView.text = obj.toString()
+        textViewDescription.text = obj.description
+        textViewLatitude.text = "${App.instance.getString(R.string.latitude_two_dots)} ${obj.latitude}"
+        textViewLongitude.text = "${App.instance.getString(R.string.longitude_two_dots)} ${obj.longitude}"
     }
 
 }
