18f323c5b210948ed6c80bdb0e0b31cb4bc55908
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-12-24 18:10:35 -0200

    domain class

---
+++ app/src/main/java/br/com/ufg/www/events/domain/BaseInteractorMVVM.kt
@@ -0,0 +1,7 @@
+package br.com.ufg.www.events.domain
+
+import kotlinx.coroutines.Job
+
+abstract class BaseInteractorMVVM(val job: Job) {
+//    abstract val callbackNetworkRequest: CallbackNetworkRequest?
+}
