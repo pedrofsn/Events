eeae53bfad236f6aa2b148b0f86c3b8d23cbbae5
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-12-26 16:10:52 -0200

    Mock + hack for easy development

--- app/src/main/java/br/com/ufg/www/events/view/user/login/LoginViewModel.kt
+++ app/src/main/java/br/com/ufg/www/events/view/user/login/LoginViewModel.kt
@@ -1,7 +1,9 @@
 package br.com.ufg.www.events.view.user.login
 
+import br.com.redcode.base.extensions.delay
 import br.com.redcode.base.utils.Constants.EMPTY_STRING
 import br.com.ufg.www.events.App
+import br.com.ufg.www.events.database.Mock
 import br.com.ufg.www.events.domain.BaseViewModelWithLiveData2
 import br.com.ufg.www.events.model.ui.InputLogin
 import kotlinx.coroutines.launch
@@ -15,13 +17,15 @@ class LoginViewModel : BaseViewModelWithLiveData2<InputLogin>() {
         temp.login = "pedrofsn"
         temp.password = "pedrofsn"
         liveData.postValue(temp)
-        login()
+        delay { login() }
     }
 
     fun login() {
         launch(coroutineContext) {
             liveData.value?.let { login ->
                 val loggedIn = interactor.login(login).await()
+
+                Mock.seedDatabase()
 
                 val command = if (loggedIn) {
                     App.userLoggedIn = login.login
