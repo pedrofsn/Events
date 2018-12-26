3a5b69c1b40045c0d9e0b6176480d2c09ce9f42b
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-12-26 16:11:28 -0200

    Watching properties dependents in two-way data binding

--- app/src/main/java/br/com/ufg/www/events/model/ui/InputEvent.kt
+++ app/src/main/java/br/com/ufg/www/events/model/ui/InputEvent.kt
@@ -4,6 +4,7 @@ import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import br.com.redcode.base.mvvm.extensions.watchBR
 import br.com.redcode.base.utils.Constants.EMPTY_STRING
+import br.com.redcode.easyvalidation.isAllValid
 import br.com.ufg.www.events.BR
 
 data class InputEvent(
@@ -24,5 +25,9 @@ data class InputEvent(
 
     @get:Bindable
     var address: String by watchBR(BR.address)
+
+    var hasLocation = false
+        @Bindable(value = ["latitude", "longitude", "address"])
+        get() = isAllValid(latitude, longitude, address)
 
 }
