4f5edd93ed0a38923209401db13abe71a48fb29a
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-12-27 11:54:21 -0200

    Solving TODO

--- app/src/main/java/br/com/ufg/www/events/view/places/list/adapter/AdapterPlace.kt
+++ app/src/main/java/br/com/ufg/www/events/view/places/list/adapter/AdapterPlace.kt
@@ -3,10 +3,9 @@ package br.com.ufg.www.events.view.places.list.adapter
 import br.com.redcode.base.mvvm.domain.adapter.BaseAdapterMVVM
 import br.com.ufg.www.events.R
 import br.com.ufg.www.events.data.model.Place
-import br.com.ufg.www.events.databinding.AdapterPlaceBinding
+import br.com.ufg.www.events.databinding.ViewholderPlaceBinding
 
-// TODO rename adapter and viewholder (including xml)
-class AdapterPlace(override var click: ((Place, Int) -> Unit)?) : BaseAdapterMVVM<Place, AdapterPlaceBinding>() {
-    override val layout: Int = R.layout.adapter_place
-    override fun getViewHolder(binding: AdapterPlaceBinding) = ViewHolderPlace(binding)
+class AdapterPlace(override var click: ((Place, Int) -> Unit)?) : BaseAdapterMVVM<Place, ViewholderPlaceBinding>() {
+    override val layout: Int = R.layout.viewholder_place
+    override fun getViewHolder(binding: ViewholderPlaceBinding) = ViewHolderPlace(binding)
 }
--- app/src/main/java/br/com/ufg/www/events/view/places/list/adapter/ViewHolderPlace.kt
+++ app/src/main/java/br/com/ufg/www/events/view/places/list/adapter/ViewHolderPlace.kt
@@ -2,9 +2,9 @@ package br.com.ufg.www.events.view.places.list.adapter
 
 import br.com.redcode.base.mvvm.domain.adapter.BaseViewHolderMVVM
 import br.com.ufg.www.events.data.model.Place
-import br.com.ufg.www.events.databinding.AdapterPlaceBinding
+import br.com.ufg.www.events.databinding.ViewholderPlaceBinding
 
-class ViewHolderPlace(binding: AdapterPlaceBinding) : BaseViewHolderMVVM<Place, AdapterPlaceBinding>(binding) {
+class ViewHolderPlace(binding: ViewholderPlaceBinding) : BaseViewHolderMVVM<Place, ViewholderPlaceBinding>(binding) {
 
     override fun bind(data: Place) {
         binding.data = data
