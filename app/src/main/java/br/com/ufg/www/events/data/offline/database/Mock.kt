7423a0ba7de8d78a23ebc64ac4be063b60c3925e
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-12-27 22:29:24 -0200

    Improving mock

--- app/src/main/java/br/com/ufg/www/events/data/offline/database/Mock.kt
+++ app/src/main/java/br/com/ufg/www/events/data/offline/database/Mock.kt
@@ -1,8 +1,8 @@
 package br.com.ufg.www.events.data.offline.database
 
 import br.com.redcode.base.extensions.toUpperCaseAndApplySHA256
-import br.com.ufg.www.events.data.offline.entities.JobTypeEntity
-import br.com.ufg.www.events.data.offline.entities.UserEntity
+import br.com.ufg.www.events.data.offline.entities.*
+import java.util.*
 
 object Mock {
 
@@ -16,8 +16,17 @@ object Mock {
     private val mockJobType2 = JobTypeEntity(id = 2, description = "Churrasqueiro")
     private val mockJobType3 = JobTypeEntity(id = 3, description = "Animador de festa infantil")
 
+    private val mockPlace = PlaceEntity(1, mockUser.id, "-16.650307", "-49.228746", "Parque Beija Flor")
+    private val mockEventEntity = EventEntity(1, mockPlace.idUser, mockPlace.id, Date(), "Festa do Dia")
+
+    private val mockEventWithJobTypeEntity1 = EventWithJobTypeEntity(1, mockEventEntity.id, mockJobType1.id)
+    private val mockEventWithJobTypeEntity2 = EventWithJobTypeEntity(2, mockEventEntity.id, mockJobType2.id)
+
     fun seedDatabase() {
         AppDatabase.getInstance().userDAO().insert(mockUser)
         AppDatabase.getInstance().jobTypesDAO().insert(mockJobType1, mockJobType2, mockJobType3)
+        AppDatabase.getInstance().placeDAO().insert(mockPlace)
+        AppDatabase.getInstance().eventDAO().insert(mockEventEntity)
+        AppDatabase.getInstance().eventWithJobTypeDAO().insert(mockEventWithJobTypeEntity1, mockEventWithJobTypeEntity2)
     }
 }
