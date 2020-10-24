35504cde106ae908c4f20d7900011468b75dad07
pedrofsn <pedrokra@gmail.com>
2020-10-24 11:14:44 -0300

    removing old project

--- app/src/main/java/br/com/ufg/www/events/data/model/User.kt
+++
@@ -1,6 +0,0 @@
-package br.com.ufg.www.events.data.model
-
-data class User(
-        val idUser: Long,
-        val email: String
-)
--- app/src/main/java/br/com/ufg/www/events/data/ui/LabelPlaces.kt
+++
@@ -1,5 +0,0 @@
-package br.com.ufg.www.events.data.ui
-
-import br.com.ufg.www.events.data.model.Place
-
-data class LabelPlaces(val items: List<Place>)
