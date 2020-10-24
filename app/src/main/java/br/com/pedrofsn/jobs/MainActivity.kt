0ce2c862991bc28aeff67e915df4a0324d69051d
pedrofsn <pedrokra@gmail.com>
2020-10-24 11:58:56 -0300

    Adding first fragment with navigation jetpack

--- app/src/main/java/br/com/pedrofsn/jobs/MainActivity.kt
+++ app/src/main/java/br/com/pedrofsn/jobs/MainActivity.kt
@@ -2,10 +2,18 @@ package br.com.pedrofsn.jobs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy { Navigation.findNavController(this, R.id.navHost) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
 }
