bdc581014b87c4d244eb9f62d0d02923aa57fd1d
pedrofsn <pedrokra@gmail.com>
2020-11-10 21:53:24 -0300

    Refactoring

--- app/src/main/java/br/com/pedrofsn/jobs/domain/BaseViewHolderMVVM.kt
+++ app/src/main/java/br/com/pedrofsn/jobs/domain/BaseViewHolderMVVM.kt
@@ -1,5 +1,6 @@
 package br.com.pedrofsn.jobs.domain
 
+import android.content.Context
 import android.view.View
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
@@ -8,15 +9,12 @@ import androidx.recyclerview.widget.RecyclerView
  * Created by pedrofsn on 16/10/2017.
  */
 
-abstract class BaseViewHolderMVVM<Data, B : ViewDataBinding>(val binding: B) :
+abstract class BaseViewHolderMVVM<Data, B : ViewDataBinding>(private val binding: B) :
     RecyclerView.ViewHolder(binding.root) {
 
+    protected fun context(): Context = binding.root.context
 
-    protected fun context() = binding.root.context
-
-    open fun bind(data: Data) {
-        binding.executePendingBindings()
-    }
+    open fun bind(data: Data) = binding.executePendingBindings()
 
     open fun bind(data: Data, onClick: ((Data, Int) -> Unit)?) {
         bind(data)
@@ -24,9 +22,7 @@ abstract class BaseViewHolderMVVM<Data, B : ViewDataBinding>(val binding: B) :
     }
 
     open fun click(view: View?, data: Data, onClick: ((Data, Int) -> Unit)?) {
-        view?.setOnClickListener {
-            onClick?.invoke(data, adapterPosition)
-        }
+        view?.setOnClickListener { onClick?.invoke(data, adapterPosition) }
     }
 
 }
