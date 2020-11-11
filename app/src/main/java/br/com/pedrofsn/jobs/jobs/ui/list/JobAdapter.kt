72f708c7f3a989fe73e8f7bdf0c7b9b7b0b9ae98
pedrofsn <pedrokra@gmail.com>
2020-11-10 21:16:23 -0300

    Creating job adapter

--- app/src/main/java/br/com/pedrofsn/jobs/jobs/ui/list/JobAdapter.kt
+++ app/src/main/java/br/com/pedrofsn/jobs/jobs/ui/list/JobAdapter.kt
@@ -1,18 +1,21 @@
 package br.com.pedrofsn.jobs.jobs.ui.list
 
-import android.view.View
 import br.com.pedrofsn.jobs.R
+import br.com.pedrofsn.jobs.databinding.ViewholderJobBinding
+import br.com.pedrofsn.jobs.domain.BaseAdapterMVVM
 import br.com.pedrofsn.jobs.jobs.data.model.JobItem
-import br.com.redcode.easyrecyclerview.library.adapter.BaseAdapter
 
 /*
     CREATED BY @PEDROFSN IN 24/10/20 12:17
 */
 
 class JobAdapter(override var click: ((JobItem, Int) -> Unit)? = null) :
-    BaseAdapter<JobItem, JobViewHolder>() {
+    BaseAdapterMVVM<JobItem, ViewholderJobBinding>() {
 
     override val layout: Int = R.layout.viewholder_job
-    override fun getViewHolder(view: View) = JobViewHolder(view)
+
+    override fun getViewHolder(binding: ViewholderJobBinding): JobViewHolder {
+        return JobViewHolder(binding)
+    }
 
 }
--- app/src/main/java/br/com/pedrofsn/jobs/jobs/ui/list/JobViewHolder.kt
+++ app/src/main/java/br/com/pedrofsn/jobs/jobs/ui/list/JobViewHolder.kt
@@ -1,23 +1,12 @@
 package br.com.pedrofsn.jobs.jobs.ui.list
 
-import android.view.View
-import android.widget.TextView
-import br.com.pedrofsn.jobs.R
+import br.com.pedrofsn.jobs.databinding.ViewholderJobBinding
+import br.com.pedrofsn.jobs.domain.BaseViewHolderMVVM
 import br.com.pedrofsn.jobs.jobs.data.model.JobItem
-import br.com.redcode.easyrecyclerview.library.adapter.viewholder.BaseViewHolder
 
 /*
     CREATED BY @PEDROFSN IN 24/10/20 12:18
 */
 
-class JobViewHolder(itemView: View) : BaseViewHolder<JobItem>(itemView) {
-
-    private val textViewTitle by lazy { itemView.findViewById<TextView>(R.id.textViewTitle) }
-    private val textViewDescription by lazy { itemView.findViewById<TextView>(R.id.textViewDescription) }
-
-    override fun populate(obj: JobItem) {
-        textViewTitle.text = obj.title
-        textViewDescription.text = obj.description
-    }
-
-}
+class JobViewHolder(binding: ViewholderJobBinding) :
+    BaseViewHolderMVVM<JobItem, ViewholderJobBinding>(binding)
