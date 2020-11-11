57a9509cd1bbe1f3dc32f02427471f62f2f0a622
pedrofsn <pedrokra@gmail.com>
2020-11-10 23:00:13 -0300

    Creating new fragment

---
+++ app/src/main/java/br/com/pedrofsn/jobs/jobs/ui/detail/JobDetailFragment.kt
@@ -0,0 +1,10 @@
+package br.com.pedrofsn.jobs.jobs.ui.detail
+
+import br.com.pedrofsn.jobs.R
+import br.com.pedrofsn.jobs.domain.BaseFragment
+
+/*
+    CREATED BY @PEDROFSN IN 10/11/20 22:57
+*/
+
+class JobDetailFragment(override val layout: Int = R.layout.fragment_job_detail) : BaseFragment()
