53d944ab543ed126d25bb80a5a66ec1567b810b3
pedrofsn <pedrokra@gmail.com>
2021-02-27 11:46:50 -0300

    Adding viewmodel declaration in DI

--- app/src/main/java/br/com/pedrofsn/jobs/di/Modules.kt
+++ app/src/main/java/br/com/pedrofsn/jobs/di/Modules.kt
@@ -2,6 +2,7 @@ package br.com.pedrofsn.jobs.di
 
 import br.com.pedrofsn.jobs.jobs.data.repository.Repository
 import br.com.pedrofsn.jobs.jobs.data.repository.RepositoryImpl
+import br.com.pedrofsn.jobs.jobs.ui.detail.JobViewModel
 import br.com.pedrofsn.jobs.jobs.ui.list.JobsViewModel
 import br.com.pedrofsn.jobs.network.API
 import br.com.pedrofsn.jobs.network.APIConnection
@@ -10,13 +11,10 @@ import br.com.pedrofsn.jobs.network.infra.Interactor
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-/*
-    CREATED BY @PEDROFSN IN 24/10/20 12:30
-*/
-
 val jobsModule = module {
     single<API> { APIConnection.service }
     factory<Interactor> { InteractorImpl(api = get()) }
     factory<Repository> { RepositoryImpl(interactor = get()) }
     viewModel { JobsViewModel(repository = get()) }
+    viewModel { JobViewModel() }
 }
