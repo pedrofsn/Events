2c16c64a9258dc9d17f5da64061f869ffb9820c6
pedrofsn <pedrokra@gmail.com>
2021-03-06 17:13:10 -0300

    Mapping new query paramater to handle pagination

--- app/src/main/java/br/com/pedrofsn/jobs/data/API.kt
+++ app/src/main/java/br/com/pedrofsn/jobs/data/API.kt
@@ -2,9 +2,10 @@ package br.com.pedrofsn.jobs.data
 
 import br.com.pedrofsn.jobs.data.payload.ResponseList
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface API {
 
     @GET("anuncios")
-    suspend fun receiveList(): ResponseList?
+    suspend fun receiveList(@Query("page") page: Int): ResponseList?
 }
